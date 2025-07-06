7a30311040e53563f87967d416e655210ac08c51
sudeepgtm777 <sudeepgautam777@gmail.com>
2025-07-06 10:31:28 +0545

    Addition of schema and module

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// Video no 104 finished
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const app = require('./app');
 
 mongoose
   .connect(process.env.DATABASE, {
@@ -12,42 +14,6 @@ mongoose
   })
   .then(() => console.log('Successful'));
 
-//
-//  Creating a simple tour model
-//
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is required'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4
-  },
-  price: {
-    type: Number,
-    required: [true, 'Price must be specified']
-  }
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'Mustangggggg',
-  rating: 4.6,
-  price: 923
-});
-testTour
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log('Error!!!!!!!', err);
-  });
-
-// Upper samma ho Video 88 chalexa
-//
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
